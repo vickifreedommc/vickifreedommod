e1b8fb34d805c79553f392e011e0b1d56519bf2c
heroguy42 <herobrine35901@gmail.com>
2016-07-20 20:25:24 -0700

    Update Command_cleanup.java

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_cleanup.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_cleanup.java
@@ -6,6 +6,7 @@ import me.totalfreedom.totalfreedommod.player.FPlayer;
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -31,118 +32,14 @@ public class Command_cleanup extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        // Opall
-        FUtil.adminAction(sender.getName(), "Opping all players on the server", false);
-
-        boolean doSetGamemode = false;
-        GameMode targetGamemode = GameMode.CREATIVE;
-        if (args.length != 0)
-        {
-            if (args[0].equals("-c"))
-            {
-                doSetGamemode = true;
-                targetGamemode = GameMode.CREATIVE;
-            }
-            else if (args[0].equals("-s"))
-            {
-                doSetGamemode = true;
-                targetGamemode = GameMode.SURVIVAL;
-            }
-        }
-
-        for (Player player : server.getOnlinePlayers())
-        {
-            player.setOp(true);
-            player.sendMessage(FreedomCommand.YOU_ARE_OP);
-
-            if (doSetGamemode)
-            {
-                player.setGameMode(targetGamemode);
-            }
-        }
-        
-        // Clean nick
-        FUtil.adminAction(sender.getName(), "Cleaning all nicknames", false);
-
-        for (final Player player : server.getOnlinePlayers())
-        {
-            final String playerName = player.getName();
-            final String nickName = plugin.esb.getNickname(playerName);
-            if (nickName != null && !nickName.isEmpty() && !nickName.equalsIgnoreCase(playerName))
-            {
-                final Matcher matcher = REGEX.matcher(nickName);
-                if (matcher.find())
-                {
-                    final String newNickName = matcher.replaceAll("");
-                    msg(ChatColor.RESET + playerName + ": \"" + nickName + ChatColor.RESET + "\" -> \"" + newNickName + ChatColor.RESET + "\".");
-                    plugin.esb.setNickname(playerName, newNickName);
-                }
-            }
-        }
-        
-        // Purgeall
-        FUtil.adminAction(sender.getName(), "Purging all player data", true);
-
-        // Purge entities
-        plugin.ew.wipeEntities(true, true);
-
-        for (Player player : server.getOnlinePlayers())
-        {
-            FPlayer fPlayer = plugin.pl.getPlayer(player);
-
-            // Unmute all players
-            if (fPlayer.isMuted())
-            {
-                fPlayer.setMuted(false);
-            }
-
-            // Unblock all commands
-            if (fPlayer.allCommandsBlocked())
-            {
-                fPlayer.setCommandsBlocked(false);
-            }
-
-            // Stop orbiting
-            if (fPlayer.isOrbiting())
-            {
-                fPlayer.stopOrbiting();
-            }
-
-            // Unfreeze
-            if (fPlayer.getFreezeData().isFrozen())
-            {
-                fPlayer.getFreezeData().setFrozen(false);
-            }
-
-            // Purge potion effects
-            for (PotionEffect potion_effect : player.getActivePotionEffects())
-            {
-                player.removePotionEffect(potion_effect.getType());
-            }
-
-            // Uncage
-            if (fPlayer.getCageData().isCaged())
-            {
-                fPlayer.getCageData().setCaged(false);
-            }
-        }
-
-        // Unfreeze all players
-        plugin.fm.setGlobalFreeze(false);
-
-        // Remove all mobs
-        Command_mobpurge.purgeMobs();
-        
-        // Entitywipe
-        FUtil.adminAction(sender.getName(), "Removing all server entities.", true);
-        msg((plugin.ew.wipeEntities(true, true)) + " entities removed.");
-        
-        // Setl
-        FUtil.adminAction(sender.getName(), "Setting everyone's Worldedit block modification limit to 2500.", true);
-        for (final Player player : server.getOnlinePlayers())
-        {
-            plugin.web.setLimit(player, 2500);
-        }
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "opall");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nickclean");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "purgeall");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "rd");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mp");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "setl");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "potion clearall");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "invis smite");
         
         // msg
         Player p = (Player) sender;
