f25ef3fe38a21741dff4fe8ad6fbb55e6f287167
vickifreedommc <vickifreedominc@gmail.com>
2016-07-20 22:19:38 -0500

    Update ChatManager.java

--- src/main/java/me/totalfreedom/totalfreedommod/ChatManager.java
+++ src/main/java/me/totalfreedom/totalfreedommod/ChatManager.java
@@ -1,5 +1,6 @@
 package me.totalfreedom.totalfreedommod;
 
+import me.totalfreedom.totalfreedommod.command.FreedomCommand;
 import me.totalfreedom.totalfreedommod.player.FPlayer;
 import me.totalfreedom.totalfreedommod.util.FLog;
 import me.totalfreedom.totalfreedommod.util.FSync;
@@ -8,6 +9,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import net.pravian.aero.util.Ips;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
